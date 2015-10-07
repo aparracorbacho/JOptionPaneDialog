ebdb7336537da1683d789b9b7d491c794ed12b48
aparracorbacho <aparracorbacho@atanasoff24.danielcastelao.org>
2015-10-07 11:23:34 +0200

    Pruebas JOptionPane Dialog

---
+++ src/joptionpanedialog/JOPtionPaneDialog.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joptionpanedialog;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author aparracorbacho
+ */
+public class JOPtionPaneDialog {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Component frame = null;
+        
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(frame, "Mensaje de ventana de dialogo");
+        /*
+        * Al crearlo hay que crear frame y definirla,, ademas de importar tanto el component como el JOptionPane
+        */
+    }
+    
+}
